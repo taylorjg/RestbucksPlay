c0a490088d4e6d0f312d92dade9a36f8e475bbe0
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-11-05 14:33:57 +0000

    LoadXmlWithSchema now returns Try[Elem]

--- app/ops/NodeSeqOps.scala
+++ app/ops/NodeSeqOps.scala
@@ -8,6 +8,7 @@ import javax.xml.validation.SchemaFactory
 
 import org.xml.sax.InputSource
 
+import scala.util.{Success, Try}
 import scala.xml.{Elem, Node, NodeSeq}
 
 object NodeSeqOps {
@@ -23,11 +24,12 @@ object NodeSeqOps {
 }
 
 object LoadXmlWithSchema {
-  def apply(xml: String, schemaResource: String): Elem = {
-    val schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
-    val schema = schemaFactory.newSchema(new StreamSource(getClass.getClassLoader.getResourceAsStream(schemaResource)))
-    val xmlStream = new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8))
-    val xmlInputSource = new InputSource(xmlStream)
-    new SchemaAwareFactoryAdapter(schema).loadXML(xmlInputSource)
-  }
+  def apply(xml: String, schemaResource: String): Try[Elem] =
+    Try {
+      val schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
+      val schema = schemaFactory.newSchema(new StreamSource(getClass.getClassLoader.getResourceAsStream(schemaResource)))
+      val xmlStream = new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8))
+      val xmlInputSource = new InputSource(xmlStream)
+      new SchemaAwareFactoryAdapter(schema).loadXML(xmlInputSource)
+    }
 }
--- test/modelSerialisation/XmlDeserialisationWithSchemaSpec.scala
+++ test/modelSerialisation/XmlDeserialisationWithSchemaSpec.scala
@@ -1,6 +1,9 @@
 package modelSerialisation
 
 import org.scalatest.{Matchers, WordSpec}
+import org.xml.sax.SAXParseException
+
+import scala.util._
 
 class XmlDeserialisationWithSchemaSpec extends WordSpec with Matchers {
 
@@ -17,13 +20,16 @@ class XmlDeserialisationWithSchemaSpec extends WordSpec with Matchers {
           |  </item>
           |</order>
         """.stripMargin
-      val elem = ops.LoadXmlWithSchema(xml, "schema/orderRequest.xsd")
-      println(s"elem: $elem")
+      val tryElem = ops.LoadXmlWithSchema(xml, "schema/orderRequest.xsd")
+      tryElem match {
+        case Success(_) =>
+        case _ => fail("Expected Success(_)")
+      }
     }
   }
 
   "deserialisation of invalid XML" should {
-    "fail" in {
+    "fail with SAXParseException" in {
       val xml =
         """
           |<order>
@@ -35,8 +41,11 @@ class XmlDeserialisationWithSchemaSpec extends WordSpec with Matchers {
           |  </item>
           |</order>
         """.stripMargin
-      val elem = ops.LoadXmlWithSchema(xml, "schema/orderRequest.xsd")
-      println(s"elem: $elem")
+      val tryElem = ops.LoadXmlWithSchema(xml, "schema/orderRequest.xsd")
+      tryElem match {
+        case Failure(ex: SAXParseException) =>
+        case _ => fail("Expected Failure(SAXParseException)")
+      }
     }
   }
 }
